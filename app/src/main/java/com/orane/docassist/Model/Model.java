eeb85123c789e2a16400f4bd30791910fe4577a4
sandhoshparamasivam <sandhoshkumar@icliniq.com>
2021-02-11 17:22:14 +0530

    Third commit

--- app/src/main/java/com/orane/docassist/Model/Model.java
+++ app/src/main/java/com/orane/docassist/Model/Model.java
@@ -13,9 +13,9 @@ import java.util.HashMap;
 
 public class Model {
     
-    public static String app_slno = "162";
-    public static String app_ver = "2.9.7";
-    public static String app_rel = "09-February-2021";
+    public static String app_slno = "163";
+    public static String app_ver = "2.9.8";
+    public static String app_rel = "11-February-2021";
 
     public static String BASE_URL = "https://www.icliniq.com/";
 //    public static String BASE_URL = "https://pentest.icliniq.com/";
